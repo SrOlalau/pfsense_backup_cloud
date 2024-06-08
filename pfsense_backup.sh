0a51736b7e53e78eaf2d3d2f3072f510f4add2be
SrOlalau <144601654+SrOlalau@users.noreply.github.com>
2024-06-08 20:47:18 +0200

    Update pfsense_backup.sh

--- pfsense_backup.sh
+++ pfsense_backup.sh
@@ -2,7 +2,7 @@
 
 # Variables to modify
 PFSENSE_IP="pfsense.example.com"  # Replace with your pfSense IP or hostname
-PFSENSE_PORT="8443"  # Replace with your pfSense web interface port
+PFSENSE_PORT="1234"  # Replace with your pfSense web interface port
 PFSENSE_USER="your_username"  # Replace with your pfSense username
 PFSENSE_PASSWORD="your_password"  # Replace with your pfSense password
 
@@ -11,11 +11,15 @@ CURRENT_DATE=$(date +%Y-%m-%d_%H-%M-%S)  # Current date and time for timestampin
 CURRENT_YEAR=$(date +%Y)  # Current year
 CURRENT_MONTH=$(date +%m)  # Current month
 CURRENT_WEEK=$(date +%V)  # Current week number
-BACKUP_ROOT_DIR="/mnt/cache/backups_pfsense"  # Root directory for backups
+BACKUP_ROOT_DIR="/your/local/path/to/save/backups"  # Root directory for backups
 BACKUP_DIR="${BACKUP_ROOT_DIR}/${CURRENT_YEAR}/${CURRENT_MONTH}/${CURRENT_WEEK}"  # Backup directory structure based on current date
 BACKUP_FILE="${BACKUP_DIR}/pfsense_config_backup_${CURRENT_DATE}.xml"  # Backup file name
 COOKIE_FILE="/tmp/pfsense_cookies.txt"  # Temporary file to store cookies
 CSRF_FILE="/tmp/pfsense_csrf.txt"  # Temporary file to store CSRF token
+
+# Paths for rclone
+SRC_PATH="${BACKUP_ROOT_DIR}"
+DESTINATION="/your/remote/path/"
 
 # Create the backup directory if it doesn't exist
 mkdir -p ${BACKUP_DIR}
@@ -61,10 +65,6 @@ notify() {
     /usr/local/emhttp/webGui/scripts/notify -i "$([[ $message == Error* ]] && echo alert || echo normal)" -s "$title ($source_path)" -d "$message" -m "$message"
   fi
 }
-
-# Paths for rclone
-SRC_PATH="/mnt/cache/backups_pfsense/"
-DESTINATION="onedrive_personal:Backups/Pfsense/"
 
 # Perform rclone operation
 rclone copy "$SRC_PATH" "$DESTINATION" -P
